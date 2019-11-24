acbbd15a69a1c98ac2fb08029b2508f7f8ce4af7
Giovanni Guidace <giovannimarassi@gmail.com>
2019-11-24 22:50:05 +0000

    Update code_mapa.py

--- Mapas e geolocalização/code_mapa.py
+++ Mapas e geolocalização/code_mapa.py
@@ -12,21 +12,23 @@ collection = db.empresas
 
 
 cnpj = []
-qtd_cnpj = []
 latitude = []
 longitude = []
 qtd_range = []
+endereco = []
 
 
-
+cnpj = db.get_collection('empresas').distinct("cnpj")
 latitude = db.get_collection('empresas').distinct("latitude")
 qtd_range = len(latitude)
 longitude = db.get_collection('empresas').distinct("longitude")
+endereco = db.get_collection('empresas').distinct("endereco")
 
-mapa = folium.Map(location=[-23.223701,-45.9009074],zoom_start=12)
+mapa = folium.Map(location=[-23.4795233,-46.2698754],zoom_start=9)
 
 for i in range(qtd_range):
-    folium.Marker([latitude[i], longitude[i]]).add_to(mapa)
+
+    folium.Marker([latitude[i], longitude[i]], popup='CNPJ: '+cnpj[i]+'\n Endereco: '+endereco[i]).add_to(mapa)
 
 
 mapa.save("mapa.html")
